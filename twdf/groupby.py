55d7828e4483296e109d9a0515ce04ba47c67285
Alexander Lyttle <43786145+alexlyttle@users.noreply.github.com>
2024-07-08 11:01:33 +0100

    Reduce joined tags, removing duplicates

--- twdf/groupby.py
+++ twdf/groupby.py
@@ -1,6 +1,10 @@
 def join_tags(tags: list[str]) -> str:
     """Join tags into a single string."""
-    return ", ".join(tags)
+    individual_tags = []
+    for t in tags:
+        individual_tags += t.split(", ")
+
+    return ", ".join(set(individual_tags))
 
 def aggregate_funcs(by: str) -> dict:
     """Get aggregation functions for groupby column."""
