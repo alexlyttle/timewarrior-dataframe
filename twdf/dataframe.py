9136ec9b98142fedb416248ae1d0d3e131b1dab1
Alexander Lyttle <43786145+alexlyttle@users.noreply.github.com>
2024-07-12 16:23:31 +0100

    Add index option

--- twdf/dataframe.py
+++ twdf/dataframe.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from timewreport.parser import TimeWarriorParser, TimeWarriorInterval
-from typing import TextIO
+from typing import TextIO, Optional
 from collections import defaultdict
 
-seconds_per_hour = 3600
+SECONDS_PER_HOUR = 3600
 
 def get_intervals(input_stream: TextIO) -> list[TimeWarriorInterval]:
     """Get Timewarrior intervals from input stream."""
@@ -21,29 +21,29 @@ def get_data(intervals: list[TimeWarriorInterval], hours_per_day: float) -> dict
         
         duration = interval.get_duration()
         data["Duration"].append(duration)
-        data["Hours"].append(duration.seconds / seconds_per_hour)
-        data["Days"].append(duration.seconds / seconds_per_hour / hours_per_day) 
+        data["Hours"].append(duration.seconds / SECONDS_PER_HOUR)
+        data["Days"].append(duration.seconds / SECONDS_PER_HOUR / hours_per_day) 
     
     return data
-
-def create_dataframe(data: dict) -> pd.DataFrame:
-    """Create a pandas DataFrame from the data."""
-    df = pd.DataFrame(data)
-    df["Week"] = df.Date.apply(lambda x: x.strftime("%W")).astype(int)
-    df["Weekday"] = df.Date.apply(lambda x: x.strftime("%a"))
-    df = df.set_index(["Week", "Date", "Weekday"], drop=True)
-    return df
 
 def explode(df: pd.DataFrame, column: str, delimiter: str=", ") -> pd.DataFrame:
     """Explode a column with comma separated values."""
     exploded_tags = df[column].str.split(delimiter).explode()
     return df.drop(columns=column).join(exploded_tags)
+
+def create_dataframe(data: dict, explode_tags: bool=False, index: Optional[list]=None) -> pd.DataFrame:
+    """Create a pandas DataFrame from the data."""
+    df = pd.DataFrame(data)
+    df["Week"] = df.Date.apply(lambda x: x.strftime("%W")).astype(int)
+    df["Weekday"] = df.Date.apply(lambda x: x.strftime("%a"))
+    if explode_tags:
+        df = explode(df, "Tags")
+    if index is not None:
+        df = df.set_index(index, drop=True)
+    return df
     
-def get_dataframe(input_stream: TextIO, hours_per_day: float, explode_tags: bool=False) -> pd.DataFrame:
+def get_dataframe(input_stream: TextIO, hours_per_day: float, explode_tags: bool=False, index: Optional[list]=None) -> pd.DataFrame:
     """Get a pandas DataFrame from the system standard input."""
     intervals = get_intervals(input_stream)
     data = get_data(intervals, hours_per_day)
-    df = create_dataframe(data)
-    if explode_tags:
-        df = explode(df, "Tags")
-    return df
+    return create_dataframe(data, explode_tags=explode_tags, index=index)
