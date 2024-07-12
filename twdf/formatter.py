7d802bcdf853722fd04bbbe481beb498a1128be7
Alexander Lyttle <43786145+alexlyttle@users.noreply.github.com>
2024-07-12 14:45:27 +0100

    Add columns option

--- twdf/formatter.py
+++ twdf/formatter.py
@@ -1,15 +1,15 @@
 import pandas as pd
+from typing import Optional
 
-
-def format_dataframe(df: pd.DataFrame, fmt: str, hours_format: str, days_format: str) -> str:
+def format_dataframe(df: pd.DataFrame, fmt: str, hours_format: str, days_format: str, columns: Optional[list]=None) -> str:
     """Format the DataFrame."""
     formatters = {
         "Hours": f"{{:{hours_format}}}".format,
         "Days": f"{{:{days_format}}}".format,
     }
     if fmt == "csv":
-        return df.to_csv()
+        return df.to_csv(columns=columns)
     elif fmt == "table":
-        return df.to_string(formatters=formatters)
+        return df.to_string(formatters=formatters, columns=columns)
     else:
         raise ValueError("Unknown format")
